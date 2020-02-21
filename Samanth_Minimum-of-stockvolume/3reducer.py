0d89f65cae366b25213268ed6b6b288e533ba324
Samanthgourineni <samanthrao226@gmail.com>
2020-02-21 14:52:33 -0600

    Completed the reducer

---
+++ Samanth_Minimum-of-stockvolume/3reducer.py
@@ -0,0 +1,30 @@
+s = open("01.txt","r")
+r = open("02.txt", "w")
+
+thisKey = ""
+thisValue = 0.0
+count =0
+
+for line in s:
+  data = line.strip().split('\t')
+  stock, date = data
+
+  if stock != thisKey:
+    if thisKey:
+      # output the last key value pair result
+      r.write(thisKey + '\t' + str(thisValue)+'\n')
+
+    # start over when changing keys
+    thisKey = stock 
+    thisValue = float(date)
+  
+  # apply the aggregation function
+  if(float(date) < float(thisValue)):
+      thisValue =float(date)
+
+# output the final entry when done
+r.write(thisKey + '\t' + str(thisValue)+'\n')
+
+s.close()
+r.close()
+

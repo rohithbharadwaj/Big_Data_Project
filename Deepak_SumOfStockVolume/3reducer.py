43cb9835634d973c4c94fa742b2f9eacd6fdd40f
DeepakJayanthi <46684265+DeepakJayanthi@users.noreply.github.com>
2020-02-21 16:19:20 -0600

    Changes in mapper and reducer files.

--- Deepak_SumOfStockVolume/3reducer.py
+++ Deepak_SumOfStockVolume/3reducer.py
@@ -1,3 +1,4 @@
+#recuder takes the input values from 01.txt and does the aggregate function and stores in the 02.txt file
 s = open("01.txt","r")
 r = open("02.txt", "w")
 
@@ -18,7 +19,8 @@ for line in s:
     thisKey = stock 
     thisValue = 0.0
   
-  # apply the aggregation function
+  
+  # apply the aggregation function which takes the sum values from eachs stock category and storing in the file 02.txt
   #if(float(stockvolume) > float(thisValue)):
     thisValue += float(stockvolume)
 

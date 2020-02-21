573b3cd0b06bf94d58ca124ea3cd59b329726db5
Samanthgourineni <samanthrao226@gmail.com>
2020-02-21 16:20:39 -0600

    Completed the commenting of mapper and reducer

--- Samanth_Minimum-of-stockvolume/1mapper.py
+++ Samanth_Minimum-of-stockvolume/1mapper.py
@@ -1,10 +1,14 @@
+# providing the input file to the mapper file
 input = open("dataset.txt", "r")
+# creating the new file to store the output values
 output = open("01.txt", "w")
 
+# Iterating the data in the loop to only create the required files.
 for line in input:
     datalist = line.strip().split("\t")
     exchange, stock, date, data, low, high, adj, stockvolume, med = datalist
-    output.write(stock + "\t" + date + "\n")
+    output.write(stock + "\t" + stockvolume + "\n")
 
+#Closing the files after storing the data
 input.close()
 output.close()
--- Samanth_Minimum-of-stockvolume/3reducer.py
+++ Samanth_Minimum-of-stockvolume/3reducer.py
@@ -1,13 +1,17 @@
+# providing the input file to the reducer file
 s = open("01.txt","r")
+# creating the new file to store the output values
 r = open("02.txt", "w")
 
 thisKey = ""
 thisValue = 0.0
 count =0
 
+
+# Iterating the data in the loop to only create the required files.
 for line in s:
   data = line.strip().split('\t')
-  stock, date = data
+  stock, stockvolume = data
 
   if stock != thisKey:
     if thisKey:
@@ -16,11 +20,11 @@ for line in s:
 
     # start over when changing keys
     thisKey = stock 
-    thisValue = float(date)
+    thisValue = float(stockvolume)
   
-  # apply the aggregation function
-  if(float(date) < float(thisValue)):
-      thisValue =float(date)
+  # applied  the aggregation function - minimum
+  if(float(stockvolume) < float(thisValue)):
+      thisValue =float(stockvolume)
 
 # output the final entry when done
 r.write(thisKey + '\t' + str(thisValue)+'\n')
